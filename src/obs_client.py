25c47d35b5893bb2bd45e60d47d35b15a8933213
Amijot Singh <amijot@berkeley.edu>
2026-01-01 17:07:49 -0800

    Created OBS connection file

---
+++ src/obs_client.py
@@ -0,0 +1,23 @@
+import obsws_python as obs
+import os 
+
+class OBSController:
+    def __init__(self):
+        self.client = obs.ReqClient(
+            host=os.getenv("OBS_HOST"),
+            port=int(os.getenv("OBS_PORT")),
+            password=os.getenv("OBS_PASSWORD")
+        )
+
+        try:
+            self.client.start_replay_buffer()
+            print("✅ Replay Started!")
+        except Exception as e:
+             print(f"❌ Error triggering OBS: {e}")
+        
+    def save_replay:
+        try:
+            self.client.save_replay_buffer()
+            print("✅ Replay Saved!")
+        except Exception as e:
+             print(f"❌ Error triggering OBS: {e}")
